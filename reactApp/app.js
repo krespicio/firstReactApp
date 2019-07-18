b2895ba95364d2f3ecbaccb3d7399c9922676593
Kyle Respicio <kyle.b.respicio@gmail.com>
2019-07-18 14:50:58 -0400

    Add a Dummy List that can be rendered

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,8 +1,46 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+// const React = require("react");
+// const ReactDOM = require("react-dom");
 
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<p>React lives!</p>,
-    document.getElementById('root'));
+let dummyList = [
+  "buy cat food",
+  "finish homework",
+  "print out assignemtns",
+  "call mom"
+];
+
+class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <li>
+        <button type="button" value="X">
+          X
+        </button>
+        {this.props.todo}
+      </li>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.props.list.map(item => {
+          return <Todo todo={item} />;
+        })}
+      </ul>
+    );
+  }
+}
+ReactDOM.render(<TodoList list={dummyList} />, document.getElementById("root"));
