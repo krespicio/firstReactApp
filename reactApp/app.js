54d3ca4b77757f34b9e2d50eecbd7ffdab605b7b
Kyle Respicio <kyle.b.respicio@gmail.com>
2019-07-18 14:18:57 -0400

    Successfully Uploaded React

---
+++ reactApp/app.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+ReactDOM.render(<p>React lives!</p>,
+    document.getElementById('root'));
